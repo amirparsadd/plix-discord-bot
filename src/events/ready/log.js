0e6d937cbbad62973d1671a716d0170d0427c0d1
AmirparsaDD <Amirparsab9@gmail.com>
2024-08-23 16:30:41 +0330

    seperated 2 different actions into 2 different files for ready event

---
+++ src/events/ready/log.js
@@ -0,0 +1,4 @@
+/** * @param {import('discord.js').Client} client */
+export default async (client) => {
+    console.log(`🤖 ${client.user.displayName} is online!`);
+}
--- src/events/ready/profile.js
+++ src/events/ready/profile.js
@@ -2,8 +2,6 @@ import { ActivityType, PresenceUpdateStatus } from 'discord.js';
 
 /** * @param {import('discord.js').Client} client */
 export default async (client) => {
-    console.log(`🤖 ${client.user.displayName} is online!`);
-
     client.user.setPresence({ status: PresenceUpdateStatus.Idle })
     client.user.setActivity({ name: "SMP Prices", type: ActivityType.Competing })
 }
